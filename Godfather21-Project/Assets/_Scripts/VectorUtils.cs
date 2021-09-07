1556d88b1e9267500516f5fbc9ab40946f2bfb6b
Cedric <cedric.chauvin99@gmail.com>
2021-09-07 17:14:10 +0200

    Start IA movement

---
+++ Godfather21-Project/Assets/_Scripts/VectorUtils.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorUtils
+{
+
+    public static Vector2 rotate(Vector2 v, float delta)
+    {
+        delta *= Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+
+}

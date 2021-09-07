2f5158ab9380432bd6c0c49040b1750d11f18944
Cedric <cedric.chauvin99@gmail.com>
2021-09-07 19:14:17 +0200

    Add Pawn battle

--- Godfather21-Project/Assets/_Scripts/Pawn.cs
+++ Godfather21-Project/Assets/_Scripts/Pawn.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseMovement : MonoBehaviour
+public class Pawn : MonoBehaviour
 {
     public Vector2 battleDirection;
-    public int maxAngleDirectionIdled;
-    public int maxAngleDirectionDirected;
-    public float timeBetweenDirectionChange;
-    public float walkSpeed;
+    public int maxAngleDirectionIdled = 120;
+    public int maxAngleDirectionDirected = 20;
+    public float timeBetweenDirectionChange = 2;
+    public float battleMaxTime = 10;
+    public float walkSpeed = 0.5f;
 
     private MOVEMENTTYPE movetype = MOVEMENTTYPE.IDLE;
     private Vector2 currentDirection;
     private float timer = 0;
+    private float battleTimer = 0;
     private Rigidbody2D rgb = null;
+    private GameObject Enemy = null;
 
     private void Start()
     {
@@ -24,8 +27,7 @@ public class BaseMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
-        //#region DEBUG
+        #region DEBUG
         //if (Input.GetMouseButtonDown(0))
         //    ChangeMoveType(MOVEMENTTYPE.REGROUP, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         //if (Input.GetMouseButtonUp(0))
@@ -34,7 +36,7 @@ public class BaseMovement : MonoBehaviour
         //    ChangeMoveType(MOVEMENTTYPE.LISTEN, Vector2.right);
         //if (Input.GetKeyUp(KeyCode.D))
         //    ChangeMoveType(MOVEMENTTYPE.IDLE);
-        //#endregion
+        #endregion
 
         switch (movetype)
         {
@@ -45,9 +47,8 @@ public class BaseMovement : MonoBehaviour
             case MOVEMENTTYPE.LISTEN:
                 rgb.velocity = currentDirection * walkSpeed;
                 break;
-            case MOVEMENTTYPE.CONTROLED:
-                break;
-            default:
+            case MOVEMENTTYPE.BATTLE:
+                Battle();
                 break;
         }
     }
@@ -64,6 +65,48 @@ public class BaseMovement : MonoBehaviour
             int r = Random.Range(-maxAngleDirectionIdled, maxAngleDirectionIdled);
             currentDirection = VectorUtils.Rotate(battleDirection, r).normalized;
             timer = Random.Range(0, timeBetweenDirectionChange);
+        }
+    }
+
+    private void Battle()
+    {
+        rgb.velocity = Vector2.zero;
+        if (tag == "Ally") {
+            battleTimer -= Time.deltaTime;
+            if (battleTimer < 0)
+            {
+                if (Random.value < 0.5)
+                    Destroy(gameObject);
+                else
+                    Destroy(Enemy);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy" && tag != "Enemy" || collision.tag == "Ally" && tag != "Ally")
+        {
+            if (Enemy)
+                Destroy(gameObject);
+            else
+            {
+                ChangeMoveType(MOVEMENTTYPE.BATTLE);
+                Enemy = collision.gameObject;
+                battleTimer = battleMaxTime;
+                rgb.velocity = Vector2.zero;
+            }
+            return;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == Enemy)
+        {
+            Enemy = null;
+            ChangeMoveType(MOVEMENTTYPE.IDLE);
         }
     }
 
@@ -86,6 +129,7 @@ public class BaseMovement : MonoBehaviour
         IDLE,
         REGROUP,
         LISTEN,
-        CONTROLED
+        CONTROLED,
+        BATTLE
     }
 }

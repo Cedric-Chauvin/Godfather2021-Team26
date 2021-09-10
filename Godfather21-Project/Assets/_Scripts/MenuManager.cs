ddc90d3aaed6a5b2a22a88f355f88cdfdd2e931a
Franci <algera.franci@gmail.com>
2021-09-10 11:20:27 +0200

    Menu select

--- Godfather21-Project/Assets/_Scripts/MenuManager.cs
+++ Godfather21-Project/Assets/_Scripts/MenuManager.cs
@@ -7,16 +7,19 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     int currentPosition = 0;
+    int previousPosition = 1;
     [SerializeField] List<Button> buttons;
     Player player0;
     Player player1;
-    [SerializeField] RectTransform cursor;
     bool cursorMoving = false;
+    [SerializeField] Sprite selectedImage;
+    [SerializeField] Sprite normalImage;
 
     private void Start()
     {
         player0 = Rewired.ReInput.players.GetPlayer(0);
         player1 = Rewired.ReInput.players.GetPlayer(1);
+        UpdateButtons();
         
     }
     private void Update()
@@ -24,6 +27,7 @@ public class MenuManager : MonoBehaviour
         if (!cursorMoving && player0.GetAxis("MovePawnVertical") <= -0.2 || player1.GetAxis("MovePawnVertical") <= -0.2)
         {
             cursorMoving = true;
+            previousPosition = currentPosition;
             currentPosition++;
             if (currentPosition == buttons.Count)
             {
@@ -36,6 +40,7 @@ public class MenuManager : MonoBehaviour
         }else if(!cursorMoving && player0.GetAxis("MovePawnVertical") >= 0.2 || player1.GetAxis("MovePawnVertical") >= 0.2)
         {
             cursorMoving = true;
+            previousPosition = currentPosition;
             currentPosition--;
             if (currentPosition == buttons.Count)
             {
@@ -57,7 +62,11 @@ public class MenuManager : MonoBehaviour
     public void UpdateButtons()
     {
         StartCoroutine(WaitForCursor());
-        cursor.position = buttons[currentPosition].GetComponent<RectTransform>().position;
+        buttons[currentPosition].image.sprite = selectedImage;
+        buttons[currentPosition].image.color = Color.white;
+        buttons[previousPosition].image.sprite = normalImage;
+        buttons[previousPosition].image.color = new Color(.8f, .8f, .8f);
+        //cursor.position = buttons[currentPosition].GetComponent<RectTransform>().position;
     }
 
     public void SelectedButtonPressed()
